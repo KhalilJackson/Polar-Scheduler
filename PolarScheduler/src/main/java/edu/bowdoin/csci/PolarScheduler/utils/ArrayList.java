8d9dde7a825248f34955d112bdf6ec40af912e73
Khalil Jackson <khalilj783@gmail.com>
2023-01-30 20:13:13 -0500

    "[Build] Update Maven Build Settings"

---
+++ PolarScheduler/src/main/java/edu/bowdoin/csci/PolarScheduler/utils/ArrayList.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package edu.bowdoin.csci.PolarScheduler.utils;
+
+/**
+ * @author khalil
+ *
+ */
+public class ArrayList {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
