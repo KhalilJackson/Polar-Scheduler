afc9a33df20b60da96f5dbd713758b5f2a61c506
Khalil Jackson <khalilj783@gmail.com>
2023-02-14 16:40:55 -0500

    [Implementation] Stubbed out the polar scheduler class

---
+++ PolarScheduler/src/main/java/edu/bowdoin/csci/PolarScheduler/scheduler/PolarScheduler.java
@@ -0,0 +1,84 @@
+package edu.bowdoin.csci.PolarScheduler.scheduler;
+
+import edu.bowdoin.csci.PolarScheduler.course.Course;
+
+public class PolarScheduler {
+	
+	private String title;
+	
+	
+	public PolarScheduler() {
+		
+		
+		
+	}
+	
+	public Course getCourseFromCatalogue(String a, String b) {
+		
+		
+		
+		return null;
+		
+	}
+	
+	public boolean addCourseToSchedule(String a, String b) {
+		
+		return true;
+	}
+	
+	public void createNewCourse(String a, String b, String c, String d, String e, String f, String g) {
+		
+		
+		
+	}
+	
+	public boolean createNewCourse(Course huh) {
+		
+		return true;
+	}
+	
+	
+	public boolean removeCourseFromSchedule(String a, String b) {
+		
+		return true;
+	}
+	
+	
+	public void resetSchedule() {
+		
+	}
+	
+	public String[] getCourseCatalogue() {
+		
+		return null;
+	}
+	
+	
+	public String[] getScheduledCourses() {
+		
+		return null;
+	}
+	
+	
+	public String[] getFullScheduledCourses() {
+		
+		return null;
+	}
+	
+	
+	public void setScheduleTitle(String a) {
+		
+		
+	}
+	
+	
+	public String getScheduleTitle() {
+		
+		return null;
+	}
+ 	
+	
+	
+	
+
+}
